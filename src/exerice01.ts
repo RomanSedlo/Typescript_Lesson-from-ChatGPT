085a6eeb41d8935c6f219ff120283f40eb4ae355
Roman Sedlář <roman.sedlar.024@pslib.com>
2025-03-01 09:13:53 +0100

    exerice03 jsem zapoměl uložit

--- src/exerice01.ts
+++ src/exerice01.ts
@@ -5,3 +5,4 @@ let arr1: Array<number> = [4, 5, 6];
 let arr2: Array<string> = ["apple", "banana"];
 let tuple: [string, number] = ["user", 42];
 let obj: {title:string,experience:number} = { title: "Developer", experience: 3 };
+//zde to hlásí problém pouze toho že jsem znovu nepoužil ty deklarované datové typy, kdybych napsal např. console.log(x) a všechny ostatní typy, tak by to bylo správně.
---
+++ src/exerice03.ts
@@ -0,0 +1,7 @@
+type User = {username:string,age:number,isAdmin:boolean}
+
+let user1: User = {username:"Alice",age:28,isAdmin:true}
+let user2: User = {username:"Bob",age:22,isAdmin:false} //bob je můj oblíbenec xDD. prosím používej ho častěji.
+
+console.log(user1,user2)
+
