222a85bd0c6859963d94ed2b174762632e42300d
Roman Sedlář <roman.sedlar.024@pslib.com>
2025-03-02 21:52:34 +0100

    nedodšláný první ukol l02 ex1

---
+++ src/l02exerices/exerice01.ts
@@ -0,0 +1,19 @@
+let temperature: number = -3
+let role: string = "admin"
+
+if (temperature<30) {
+    if(temperature<0) {
+        console.log("Freezing")
+    }
+    else {
+        console.log("Normal")
+    }
+} else {
+    console.log("Hot")
+}
+
+switch (role) {
+    case "admin":
+        // to do - finish
+        break;
+}
